746355740805e139b50d73fb22f2e4da296d7fbe
huoqishi <wangchaohua110@foxmail.com>
2018-01-16 01:35:09 +0800

    :cn:

---
+++ ssr-server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const Renderer = require('docsify-server-renderer')
+const ssrConfig = require('./ssr.config')
+const readFileSync = require('fs').readFileSync
+const app = express()
+const renderer = new Renderer({
+  template: readFileSync('./ssr.html', 'utf-8'),
+  config: ssrConfig
+})
+// app.use(express.static(''))
+app.use((req, res) => {
+  console.log(req.url)
+  renderer.renderToString(req.url)
+    .then(html => {
+      res.send(html)
+    })
+    .catch(err => {
+      if (err) {
+        res.send('error')
+      }
+    })
+})
+app.listen(8091, '127.0.0.1', err => {
+  if (err) return console.log('出错了', err)
+  console.log('监听成功: http://127.0.0.1:8091')
+})
+
+process.on('uncaughtException', function (err) {
+  console.log('Caught exception: ' + err)
+})
--- ssr.config.js
+++ ssr.config.js
@@ -1,35 +1,43 @@
+const config = {
+  // basePath: './docs',
+  // basePath: 'http://huoqishi.github.io/vuejs-docs/docs',
+  basePath: 'http://huoqishi.oschina.io/vuejs-docs/docs',
+  alias: {
+    '/de-de/changelog': '/changelog',
+    '/zh-cn/changelog': '/changelog'
+    // '/changelog': 'https://raw.githubusercontent.com/QingWei-Li/docsify/master/CHANGELOG'
+  },
+  auto2top: true,
+  // coverpage: true,
+  executeScript: true,
+  loadSidebar: './$_sidebar.md',
+  coverpage: './$_coverpage.md',
+  // loadSidebar: true,
+  // loadNavbar: true,
+  homepage: './home.md',
+  mergeNavbar: true,
+  maxLevel: 4,
+  subMaxLevel: 2,
+  name: 'vuejs',
+  repo: 'https://github.com/huoqishi/vuejs-docs'
+
+  // search: {
+  //   noData: {
+  //     '/de-de/': 'Keine Ergebnisse!',
+  //     '/zh-cn/': '没有结果!',
+  //     '/': 'No results!'
+  //   },
+  //   paths: 'auto',
+  //   placeholder: {
+  //     '/de-de/': 'Suche',
+  //     '/zh-cn/': '搜索',
+  //     '/': 'Search'
+  //   }
+  // }
+}
 module.exports = {
   template: './ssr.html',
   maxAge: 60 * 60 * 1000,
-  config: {
-    basePath: 'http://vuejs.huoqishi.net/docs',
-    alias: {
-      '/de-de/changelog': '/changelog',
-      '/zh-cn/changelog': '/changelog',
-      '/changelog': 'https://raw.githubusercontent.com/QingWei-Li/docsify/master/CHANGELOG'
-    },
-    auto2top: true,
-    coverpage: true,
-    executeScript: true,
-    loadSidebar: true,
-    loadNavbar: true,
-    homepage: './home.md',
-    mergeNavbar: true,
-    maxLevel: 4,
-    subMaxLevel: 2,
-    name: 'vuejs',
-    search: {
-      noData: {
-        '/de-de/': 'Keine Ergebnisse!',
-        '/zh-cn/': '没有结果!',
-        '/': 'No results!'
-      },
-      paths: 'auto',
-      placeholder: {
-        '/de-de/': 'Suche',
-        '/zh-cn/': '搜索',
-        '/': 'Search'
-      }
-    }
-  }
+  config
 }
+module.exports = config
